5ec7cda41c5ce2dbb295a3080e5ccdc95c359a57
XieChengG <xiechenggang@yeah.net>
2019-02-26 22:41:17 +0800

    add file of game settings module

---
+++ settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+class Settings(object):
+    """
+    用于存储游戏的所有的设置
+    """
+    def __init__(self):
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+
+
+
+
+
